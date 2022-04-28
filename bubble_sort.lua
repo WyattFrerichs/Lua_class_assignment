c409ca8631f8ca93a96e84a9288d0012c718cffe
Wyatt Frerichs <89824728+WyattFrerichs@users.noreply.github.com>
2022-04-28 02:32:43 +0000

    Initial commit

---
+++ bubble_sort.lua
@@ -0,0 +1,25 @@
+function make_arr()
+  local out = {}
+  for i=1,100 do
+  out[i]=math.random(1000)
+  end
+  return out
+end
+
+x = make_arr()
+local size = table.getn(x)
+local temp = {}
+
+for i= 1,size do
+  for j= 1,size-i do
+    if x[j]>x[j+1] then
+      temp[0] = x[j+1]
+      x[j+1] = x[j]
+      x[j] = temp[0]
+    end
+  end
+end
+
+for i=1,100 do
+  print(x[i])
+end
